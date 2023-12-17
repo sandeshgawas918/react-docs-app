8364e18b6f717948553b905004698f824046a473
sandesh gawas <sandeshgawas918@gmail.com>
2023-12-17 20:43:04 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import { FaRegFileAlt } from "react-icons/fa";
 
 const Card = () => {
   return (
-    <div className=' absolute bg-black w-52 h-72 rounded-lg p-7'>
+    <div className=' flex flex-col bg-black w-52 h-72 rounded-lg p-7'>
         <FaRegFileAlt className=' text-red-700'/>
         <p className=' text-yellow-500 mt-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, omnis.</p>
-        <footer className=' bg-slate-100 bottom-0'>dfdsvdsdsf</footer>
+        <footer className='bg-slate-100 self-end bottom-0 '>dfdsvdsdsf</footer>
     </div>
   )
 }
