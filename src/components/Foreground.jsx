5bbd2b6ab373cc9aa3f5ed3c2488cabc83c303ab
sandesh gawas <sandeshgawas918@gmail.com>
2023-12-19 21:44:36 +0530

    new

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -4,7 +4,12 @@ import Card from './Card'
 const Foreground = () => {
   return (
     <div className=' absolute bg-orange-500/30 w-full h-screen z-20 p-11'>
-      <Card/>
+      {/* <Card/> */}
+      <div className=' absolute bg-black w-52 h-72 rounded-lg p-7'>
+        <FaRegFileAlt className=' text-red-700' />
+        <p className=' text-yellow-500 mt-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, omnis.</p>
+        <footer className='bg-slate-100 absolute bottom-0 '>dfdsvdsdsf</footer>
+      </div>
     </div>
   )
 }
